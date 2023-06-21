4a4ec4a8221d52ec616e5532343f90d02715f79d
Navojith <navojith22@gmail.com>
2023-06-21 19:22:48 +0530

    Feat: timeline zigzag

--- src/components/ourVision.jsx
+++ src/components/ourVision.jsx
@@ -1,9 +1,23 @@
+import { FaNpm } from "react-icons/fa";
+import { TbBrandVscode } from "react-icons/tb";
+import { RxDotFilled } from "react-icons/rx";
+
 const timeLine = (data) => {
   return (
-    <div key={data.id}>
-      <img src={data.imgSrc} alt={data.imgAlt} />
-      <span>.</span>
-      <div>
+    <div key={data.id} className="my-5 flex">
+      <div className={data.logoFirst ? "w-2/5 justify-end" : "w-2/5 order-last"}>
+        {data.imgType === "npm" ? (
+          <FaNpm className={data.logoFirst ? "text-4xl ml-auto" : "text-4xl mr-auto"} />
+        ) : data.imgType === "vscode" ? (
+          <TbBrandVscode className={data.logoFirst ? "text-2xl ml-auto" : "text-4xl mr-auto"} />
+        ) : (
+          <></>
+        )}
+      </div>
+      <div className="w-1/5 ">
+        <RxDotFilled className="text-4xl mx-auto border-2 border-dashed rounded-full" />
+      </div>
+      <div className={data.logoFirst ? "w-2/5 text-start" : "w-2/5 text-right order-first"}>
         <p>{data.details}</p>
         <p>{data.date}</p>
       </div>
@@ -14,15 +28,14 @@ const timeLine = (data) => {
 const OurVision = ({ dataList }) => {
   return (
     <>
-      {console.log(dataList)}
-      <div>
+      <div className="p-5 text-center">
         <h1>Our Vision</h1>
         <p>
           A highly portable and versatile plugin ecosystem which will enable fine-grained tracing in your Javascript
           source code
         </p>
       </div>
-      <div>{dataList && dataList.map((data) => timeLine(data))}</div>
+      <div className="p-5 text-center">{dataList && dataList.map((data) => timeLine(data))}</div>
     </>
   );
 };
