4edf15069e1b9ffb89e1d55950afa320b55e6230
Navojith <navojith22@gmail.com>
2023-06-22 13:36:08 +0530

    Feat: vision basic styles

--- src/components/ourVision.jsx
+++ src/components/ourVision.jsx
@@ -4,8 +4,8 @@ import { RxDotFilled } from "react-icons/rx";
 
 const timeLine = (data) => {
   return (
-    <div key={data.id} className="my-5 flex">
-      <div className={data.logoFirst ? "w-2/5 justify-end" : "w-2/5 order-last"}>
+    <div key={data.id} className="my-10 flex">
+      <div className={data.logoFirst ? "w-1/2 justify-end" : "w-1/2 order-last"}>
         {data.imgType === "npm" ? (
           <FaNpm className={data.logoFirst ? "text-4xl ml-auto" : "text-4xl mr-auto"} />
         ) : data.imgType === "vscode" ? (
@@ -14,12 +14,12 @@ const timeLine = (data) => {
           <></>
         )}
       </div>
-      <div className="w-1/5 ">
-        <RxDotFilled className="text-4xl mx-auto border-2 border-dashed rounded-full bg-white" />
+      <div className="w-20 ">
+        <RxDotFilled className="text-[#0143b6] text-4xl mx-auto border-2 border-dashed rounded-full bg-white" />
       </div>
-      <div className={data.logoFirst ? "w-2/5 text-start" : "w-2/5 text-right order-first"}>
-        <p>{data.details}</p>
-        <p>{data.date}</p>
+      <div className={data.logoFirst ? "w-1/2 text-start" : "w-1/2 text-right order-first"}>
+        <p className="font-CabinetGroteskVariable text-xl font-extrabold">{data.details}</p>
+        <p className="text-black/40 font-Inter">{data.date}</p>
       </div>
     </div>
   );
@@ -29,8 +29,8 @@ const OurVision = ({ dataList }) => {
   return (
     <>
       <div className="p-5 text-center">
-        <h1>Our Vision</h1>
-        <p>
+        <h1 className="font-CabinetGroteskVariable font-bold text-5xl">Our Vision</h1>
+        <p className="font-Inter text-black/40 text-xl">
           A highly portable and versatile plugin ecosystem which will enable fine-grained tracing in your Javascript
           source code
         </p>
@@ -38,7 +38,21 @@ const OurVision = ({ dataList }) => {
 
       <div className="relative">
         <div className="w-1/2 min-h-full absolute inline-block border-r-2 z-0 border-dashed"></div>
-        <div className="p-5 text-center z-10 relative">{dataList && dataList.map((data) => <>{timeLine(data)}</>)}</div>
+        <div className="p-5 text-center z-10 relative">
+          {dataList && dataList.map((data) => <>{timeLine(data)}</>)}
+          <div className="bg-white">
+            <p
+              style={{
+                background: "-webkit-linear-gradient(#00299c, #0073e6)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent"
+              }}
+              className="inline-block font-bold border-2 border-dashed py-1 px-3 bg-white rounded-full"
+            >
+              Continue to Grow
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        CabinetGroteskVariable: ["Cabinet Grotesk Variable", "sans"],
+        Inter: ["Inter", "sans"]
+      }
+    }
   },
   plugins: []
 };
