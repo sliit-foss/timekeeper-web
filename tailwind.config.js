aa31f729ce11123e4574347c2566b91f428e7b52
Akalanka <akalankaperera128@gmail.com>
2023-06-27 18:09:42 +0530

    Chore: moved fonts to public directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundClip: "text",
-      colors: {
-        gl: "#00299C",
-        gr: "#0073E6"
-      },
       fontFamily: {
-        sf: ["SF Pro Display"],
         cabinet: ["Cabinet Grotesk Variable"],
-        inter: ["Inter"]
-      }
-    },
-    container: {
-      center: true,
-      screens: {
-        lg: "1160px"
+        inter: ["Inter"],
+        sf: ["SF Pro Display"]
       }
     }
   },
