9a5e3586b701039e6d75962c6353fbc7b2e8ced9
QuintusJoyal <qjoyal@gmail.com>
2023-07-02 15:29:26 +0530

    Fix(timeline): centered icons

--- src/components/landing/vision/timeline.jsx
+++ src/components/landing/vision/timeline.jsx
@@ -8,12 +8,12 @@ const Timeline = () => {
     <>
       {timeline?.map(({ icon, details, date }, index) => (
         <div className="my-10 grid grid-cols-[270px_104px_270px] justify-center" key={index}>
-          <div className={`items-center ${ index % 2 == 0 ? "md:order-2 grid place-items-end" : "md:justify-start md:order-4" }`} >
+          <div className={`items-center grid ${ index % 2 == 0 ? "md:order-2 place-items-end" : "md:order-4" }`} >
             {icon === "npm" && <NPM width={63} />}
             {icon === "vscode" && <VSCode width={24}/>}
           </div>
           <div className="my-auto grid place-items-center md:order-3">
-            <RxDotFilled className="text-primary-dark text-3xl mx-auto border-2 border-dashed rounded-full bg-white" />
+            <RxDotFilled className="text-primary-dark text-[24px] mx-auto border-2 border-dashed rounded-full bg-white" />
           </div>
           <div className={index % 2 == 0 ? "text-start order-4" : "place-items-end text-end order-2"}>
             <p className="leading-[26px] font-cabinet text-[26px] tracking-[-0.5px] font-bold">{details}</p>
