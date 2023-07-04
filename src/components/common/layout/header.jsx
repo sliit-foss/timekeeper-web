0abd10bdd9eaa38b35cb0cd80737d47ddeefb78b
aishwarya-mali <aishwaryamali29@gmail.com>
2023-07-04 17:07:52 +0530

    Feat: replaced X icon

--- src/components/common/layout/header.jsx
+++ src/components/common/layout/header.jsx
@@ -1,12 +1,12 @@
-import { FaChevronRight, FaGithub, FaTimes } from "react-icons/fa";
-import { FossLogoDark, Timekeeper } from "@/icons";
+import { FaChevronRight, FaGithub } from "react-icons/fa";
+import { FossLogoDark, Timekeeper, Times } from "@/icons";
 
 const Header = ({ className }) => {
   return (
     <header className={`items-center grid grid-flow-col py-[30px] ${className}`}>
       <div className="grid place-content-start grid-flow-col items-center space-x-[30px]">
         <Timekeeper height="46px" width="52px" />
-        <FaTimes className="opacity-20" />
+        <Times height="15px" width="15px" className="opacity-20" />
         <FossLogoDark />
       </div>
       <div className="grid place-content-end grid-flow-col items-center">
