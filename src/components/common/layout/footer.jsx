8d26964e58b2a2e47eaaa0d7c8b8dbb0c17c4939
shabinaf <shabeena.farveen@rootcodelabs.com>
2023-07-08 01:37:16 +0530

    fix: link footer socials with actual pages

--- src/components/common/layout/footer.jsx
+++ src/components/common/layout/footer.jsx
@@ -9,6 +9,7 @@ import {
 } from "react-icons/ai";
 import { Button } from "@/components/common";
 import { FossLogoDark, Timekeeper } from "@/icons";
+import socials from "@/resource/links.js";
 
 const Footer = ({ className }) => {
   return (
@@ -22,19 +23,19 @@ const Footer = ({ className }) => {
           </p>
           <div className="font-bold text-[20px]">Connect with us</div>
           <div className="flex space-x-2 flex-shrink-0">
-            <a href="#">
+            <a href={socials.facebook}>
               <AiFillFacebook />
             </a>
-            <a href="#">
+            <a href={socials.instagram}>
               <AiFillInstagram />
             </a>
-            <a href="#">
+            <a href={socials.twitter}>
               <AiOutlineTwitter />
             </a>
-            <a href="#">
+            <a href={socials.linkedIn}>
               <AiFillLinkedin />
             </a>
-            <a href="#">
+            <a href={socials.youTube}>
               <AiFillYoutube />
             </a>
           </div>
@@ -49,15 +50,19 @@ const Footer = ({ className }) => {
 
         <div className="md:col-start-3 md:row-start-1 col-start-1 md:mt-0 mt-10 row-start-2 col-span-2 grid grid-rows-4 md:place-content-end min-h-0 min-w-0">
           <div className="font-bold text-[20px] row-start-1">Visit us on</div>
+            <a href={socials.officalWebsite}>
           <Button className="md:w-3/4 h-8 bg-transparent border font-medium py-[0.1rem] gap-x-2">
             <span className="gradient-text text-[1rem]">sliitfoss.org</span>
             <AiOutlineArrowUp className="text-primary-light rotate-45" />
           </Button>
+            </a>
           <div className="font-bold text-[20px] row-start-3">Open with source</div>
+          <a href={socials.github}>
           <Button className="md:w-3/4 h-8 bg-transparent border font-medium gap-x-2">
             <span className="gradient-text text-[1rem]">Visit at</span>
             <AiFillGithub className="text-black" />
           </Button>
+          </a>
         </div>
       </div>
       <div className="border-t-2 border-dashed absolute inset-x-0 mt-7"></div>
