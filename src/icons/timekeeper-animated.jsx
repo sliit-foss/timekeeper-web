c53888fd6139cfa48a00926b8450e616331f8ea9
Akalanka <akalankaperera128@gmail.com>
2023-07-08 15:31:39 +0530

    Fix(animation): timekeeper icon rotation origin

--- src/icons/timekeeper-animated.jsx
+++ src/icons/timekeeper-animated.jsx
@@ -3,7 +3,7 @@ import { Timekeeper } from ".";
 
 const TimekeeperAnimated = ({ size = 310, className, ...props }) => {
   return (
-    <div className={twMerge("animate-[spin_10s_linear_infinite] duration-500 origin-[43%_50%]", className)}>
+    <div className={twMerge("animate-[spin_10s_linear_infinite] duration-500 origin-[43.5%_50%]", className)}>
       <Timekeeper width={size} height={size} {...props} />
     </div>
   );
