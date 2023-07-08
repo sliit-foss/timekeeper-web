8dd7cfa50d4d0344ba637a3345e48d6cf8fbeb92
Akalanka <akalankaperera128@gmail.com>
2023-07-09 01:28:19 +0530

    Feat(console): added title content

--- src/components/landing/console/index.jsx
+++ src/components/landing/console/index.jsx
@@ -6,8 +6,8 @@ const Console = ({ className }) => {
     <div className={`py-[60px] w-full ${className}`}>
       <Heading className="px-5 mb-6">See the time in real-time</Heading>
       <SubHeading className="px-5 md:px-12 lg:px-40 pb-5">
-        A highly portable and versatile plugin ecosystem which will enable fine-grained tracing in your Javascript
-        source code
+        Gain visual insight into the chronological flow of functions and unfold the inner workings of your application
+        with ease
       </SubHeading>
       <TerminalGroup className="w-[870px] mx-auto mt-[20px]" primary={code} secondary={output} />
       <Button className="mx-auto mt-[40px]" arrow>
