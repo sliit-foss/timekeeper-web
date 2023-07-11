69654d9a31732260f364d477ef80c37e5f3531f3
Akalanka <akalankaperera128@gmail.com>
2023-07-11 11:55:27 +0530

    Fix(build): issue in top level await

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { default as react } from "@vitejs/plugin-react";
 import { default as path } from "path";
 import { defineConfig } from "vite";
+import { default as topLevelAwait } from "vite-plugin-top-level-await";
 
 export default defineConfig(async () => {
   const mdx = await import("@mdx-js/rollup");
   return {
-    plugins: [react(), mdx.default()],
+    plugins: [
+      topLevelAwait({
+        promiseExportName: "__tla",
+        promiseImportName: (i) => `__tla_${i}`
+      }),
+      react(),
+      mdx.default()
+    ],
     base: "./",
     resolve: {
       alias: {
