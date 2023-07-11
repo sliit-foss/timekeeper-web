12484a8da11c01334043aebf87dce8ad54d323df
Akalanka <akalankaperera128@gmail.com>
2023-07-11 20:48:31 +0530

    Fix: removed top level await

--- src/pages/docs/index.jsx
+++ src/pages/docs/index.jsx
@@ -2,21 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import { BreadCrumbs, Explorer, Navigation } from "@/components/docs";
 import meta from "./meta.json";
 
-const pages = import.meta.glob("./**/**.mdx");
+const pages = import.meta.glob("./**/**.mdx", { eager: true });
 
-const routes = await Promise.all(
-  Object.entries(pages).map(async ([path, module]) => {
-    return module().then((resolvedModule) => {
-      return (
-        <Route
-          key={path}
-          path={path.replace("./", "").replace(".mdx", "")}
-          element={<div className="px-6 2xl:px-7 pb-7 markdown">{resolvedModule.default()}</div>}
-        />
-      );
-    });
-  })
-);
+const routes = Object.entries(pages).map(([path, page]) => {
+  return (
+    <Route
+      key={path}
+      path={path.replace("./", "").replace(".mdx", "")}
+      element={<div className="px-6 2xl:px-7 pb-7 markdown">{page.default()}</div>}
+    />
+  );
+});
 
 const Docs = () => {
   return (
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { default as react } from "@vitejs/plugin-react";
 import { default as path } from "path";
 import { defineConfig } from "vite";
-import { default as topLevelAwait } from "vite-plugin-top-level-await";
 
 export default defineConfig(async () => {
   const mdx = await import("@mdx-js/rollup");
   return {
     plugins: [
-      topLevelAwait({
-        promiseExportName: "__tla",
-        promiseImportName: (i) => `__tla_${i}`
-      }),
       mdx.default({
         remarkPlugins: []
       }),
