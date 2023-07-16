abf5f9a6477c52e48e6e2631c21b607319b15ff2
shabinaf <shabeena.farveen@rootcodelabs.com>
2023-07-16 10:08:47 +0530

    fix: add socila links to a barrel file

--- src/constants/socials.js
+++ src/constants/socials.js
@@ -1,7 +1,6 @@
-export var facebook = "https://www.facebook.com/sliitfoss";
-export var twitter = "https://twitter.com/fosssliit";
-export var instagram = "https://www.instagram.com/sliitfoss/";
-export var linkedIn = "https://www.linkedin.com/company/sliit-foss-community";
-export var youTube = "https://youtube.com/@SLIITFOSSCommunity";
-export var github = "https://github.com/sliit-foss";
-export var officalWebsite = "https://sliitfoss.org/";
+export const facebook = "https://www.facebook.com/sliitfoss";
+export const twitter = "https://twitter.com/fosssliit";
+export const instagram = "https://www.instagram.com/sliitfoss/";
+export const linkedIn = "https://www.linkedin.com/company/sliit-foss-community";
+export const youTube = "https://youtube.com/@SLIITFOSSCommunity";
+export const github = "https://github.com/sliit-foss";
