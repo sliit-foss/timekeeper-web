fe5d7aabbfeea0dcb910181bfb2c73c49d793cb5
5.H.4.D.0.W <qjoyal@gmail.com>
2023-07-28 00:05:51 +0530

    Fix(console): mobile responsiveness

--- src/components/landing/console/index.jsx
+++ src/components/landing/console/index.jsx
@@ -5,11 +5,11 @@ const Console = ({ className }) => {
   return (
     <div className={`py-[60px] w-full ${className}`}>
       <Heading className="px-5 mb-6">See the time in real-time</Heading>
-      <SubHeading className="px-5 md:px-12 lg:px-40 pb-5">
+      <SubHeading className="px-10 md:px-12 lg:px-40 pb-5">
         Gain visual insight into the chronological flow of functions and unfold the inner workings of your application
         with ease
       </SubHeading>
-      <TerminalGroup className="w-[870px] mx-auto mt-[20px]" primary={code} secondary={output} />
+      <TerminalGroup className="md:w-[calc(85%+80px)] md:mx-auto mx-10 justify-center mt-[20px]" primary={code} secondary={output} />
       <Button to="/docs/installation" className="mx-auto mt-[20px]" arrow>
         Try it out
       </Button>
